3ba53cb9237767b92a8a020640aee95f0b6b11ad
Jay Nguyen <jay.nguyen2015@gmail.com>
2022-06-14 00:02:21 +0000

    Fixed the space of fullName

--- prep-javascript-objects/main.js
+++ prep-javascript-objects/main.js
@@ -7,7 +7,7 @@ var person = {
 };
 console.log(person);
 
-var fullName = person.firstName + person.lastName;
+var fullName = person.firstName + ' ' + person.lastName;
 console.log("The person's name is:", fullName);
 
 var job = person.job;
