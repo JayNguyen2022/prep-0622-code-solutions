783799b40908220a8680a490bcb0c989567d1dc0
Jay Nguyen <jay.nguyen2015@gmail.com>
2022-06-16 11:54:31 +0000

    Completed prep-javascript-for-loops exercise

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,17 @@
+
+// create your loops here.
+var number = '';
+
+for (var number = 0; number < 10; number++) {
+  console.log(number);
+}
+
+var i = '';
+for (var i = 0; i < 18; i += 2) {
+  console.log(i);
+}
+
+var p = '';
+for (var p = 100; p > 0; p--) {
+  console.log('\'Time till explosion:', p);
+}
